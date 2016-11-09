2a45cbefc33100de1417fbdff1ba2bacb6da3388
Ehsun Behravesh <ehsun.behravesh@openet.com>
2016-11-09 11:14:45 +0800

    delete old structure.

--- src/main/java/com/openet/labs/ml/autoscale/scale/Scaler.java
+++
@@ -1,6 +0,0 @@
-
-package com.openet.labs.ml.autoscale.scale;
-
-public abstract class Scaler {      
-    public abstract Object scale(Scalable scalable);        
-}
--- src/test/java/com/openet/labs/ml/autoscale/IntegrationTest.java
+++
@@ -1,5 +0,0 @@
-package com.openet.labs.ml.autoscale;
-
-public interface IntegrationTest {
-
-}
