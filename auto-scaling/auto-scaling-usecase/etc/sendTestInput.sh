e2abc41c35b89745eece96ef22345f2f3904b2ec
subbarao <subbarao.thirumoorthy@openet.com>
2016-11-11 13:40:17 +0800

    updated sendTestInput

--- auto-scaling/auto-scaling-usecase/etc/sendTestInput.sh
+++ auto-scaling/auto-scaling-usecase/etc/sendTestInput.sh
@@ -2,4 +2,4 @@
 
 KAFKA_PATH=/home/openet/Programs/kafka_2.11-0.9.0.0/bin
 
-$KAFKA_PATH/kafka-console-producer.sh --broker-list localhost:9092 --topic com.openet.autoscaling.train < sample_input.json
+$KAFKA_PATH/kafka-console-producer.sh --broker-list localhost:9092 --topic com.openet.autoscaling < sample_input.json
